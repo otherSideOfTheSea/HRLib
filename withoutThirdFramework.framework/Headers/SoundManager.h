cf71931983db1fd3a080cfab79704b4914edad49
libaibo <libaibo87@163.com>
2016-08-18 11:32:24 +0800

    framework

---
+++ withoutThirdFramework.framework/Headers/SoundManager.h
@@ -0,0 +1,19 @@
+//
+//  SoundManager.h
+//  withoutThirdFramework
+//
+//  Created by li on 16/8/17.
+//  Copyright © 2016年 li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <UIKit/UIKit.h>
+#import <AFNetworking/AFNetworking.h>
+
+@interface SoundManager : NSObject
+
++(void)playsound;
+
++(void)test;
+@end
---
+++ withoutThirdFramework.framework/Headers/withoutThirdFramework.h
@@ -0,0 +1,20 @@
+//
+//  withoutThirdFramework.h
+//  withoutThirdFramework
+//
+//  Created by li on 16/8/17.
+//  Copyright © 2016年 li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for withoutThirdFramework.
+FOUNDATION_EXPORT double withoutThirdFrameworkVersionNumber;
+
+//! Project version string for withoutThirdFramework.
+FOUNDATION_EXPORT const unsigned char withoutThirdFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <withoutThirdFramework/PublicHeader.h>
+
+
+#import "SoundManager.h"
